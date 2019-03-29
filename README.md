476f10ca9317e24bb265488972c7b5528e4fc370
921295877@qq.com <lipengju921295877>
2019-03-29 19:27:51 +0800

    增加javaExampleGenerator节点

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# customer-mybaits-generator
-自定义Mybatis代码生成器
+#自定义Mybatis代码生成器
+mybatis-generator-core模块是修改后的源码，修改点：添加javaExampleGenerator标签用于修改example实体类的位置
+原理：修改源码增加javaExampleGenerator标签的读取和生成逻辑，修改dtd文件添加javaExampleGenerator节点
+
+#拓展的插件
+1.自动生成swigger2注解
+2.自动生成@mapper注解
+3.增加批量插入，批量更新，查询单条方法
+
+#使用方法
+1.项目拉到本地
+2.编译工具配置好环境(本地能连到外网)
+3.分别打包mybatis-generator,mybatis-generator模块
+4.修改mybatis-generator-boot模块下的generatorConfig.properties文件和generatorConfig.xml对应的表
+5.使用插件generate运行
---
+++ mybatis-generator/src/main/java/cn/lpj/mybatis/generator/OverwriteXmlPlugin.java
@@ -0,0 +1,32 @@
+package cn.lpj.mybatis.generator;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * @author LiPengJu
+ * @date 2019/3/26
+ */
+public class OverwriteXmlPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = GeneratedXmlFile.class.getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return super.sqlMapGenerated(sqlMap, introspectedTable);
+    }
+}
